b390ab390e443125989cf11fe030ddec482e54fc
appurajacool2015 <appurajacool2015@gmail.com>
2022-07-24 17:05:27 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ docker build -t appurajacool2015/springbootpostgresrestapidemo .
 # we can docker login as a initial step
 #docker login
 docker push appurajacool2015/springbootpostgresrestapidemo
-kubectl apply -f postgres-secrets.yml
+kubectl apply -f postgres-secrets.yaml
 kubectl apply -f postgres-storage.yaml
 kubectl apply -f postgres-deployment.yaml
 kubectl apply -f postgres-service.yaml
 kubectl create configmap hostname-config --from-literal=postgres_host=$(kubectl get svc postgres -o jsonpath="{.spec.clusterIP}")
-kubectl apply -f springboot-deployment.yml
-kubectl apply -f springboot-service.yml
+kubectl apply -f springboot-deployment.yaml
+kubectl apply -f springboot-service.yaml
 
